a168d1a19ccd096b1c30ac3ac332a3bd968e7237
va-kiet <voanhkiet1109@gmail.com>
2023-04-26 20:27:17 +0700

    Add new project wake word model

---
+++ AI-Voice-Assistant/wakeword/engine.py
@@ -0,0 +1,159 @@
+"""the interface to interact with wakeword model"""
+import pyaudio
+import threading
+import time
+import argparse
+import wave
+import torchaudio
+import torch
+import numpy as np
+from neuralnet.dataset import get_featurizer
+from threading import Event
+
+
+class Listener:
+
+    def __init__(self, sample_rate=8000, record_seconds=2):
+        self.chunk = 1024
+        self.sample_rate = sample_rate
+        self.record_seconds = record_seconds
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=pyaudio.paInt16,
+                        channels=1,
+                        rate=self.sample_rate,
+                        input=True,
+                        output=True,
+                        frames_per_buffer=self.chunk)
+
+    def listen(self, queue):
+        while True:
+            data = self.stream.read(self.chunk , exception_on_overflow=False)
+            queue.append(data)
+            time.sleep(0.01)
+
+    def run(self, queue):
+        thread = threading.Thread(target=self.listen, args=(queue,), daemon=True)
+        thread.start()
+        print("\nWake Word Engine is now listening... \n")
+
+
+class WakeWordEngine:
+
+    def __init__(self, model_file):
+        self.listener = Listener(sample_rate=8000, record_seconds=2)
+        self.model = torch.jit.load(model_file)
+        self.model.eval().to('cpu')  #run on cpu
+        self.featurizer = get_featurizer(sample_rate=8000)
+        self.audio_q = list()
+
+    def save(self, waveforms, fname="wakeword_temp"):
+        wf = wave.open(fname, "wb")
+        # set the channels
+        wf.setnchannels(1)
+        # set the sample format
+        wf.setsampwidth(self.listener.p.get_sample_size(pyaudio.paInt16))
+        # set the sample rate
+        wf.setframerate(8000)
+        # write the frames as bytes
+        wf.writeframes(b"".join(waveforms))
+        # close the file
+        wf.close()
+        return fname
+
+
+    def predict(self, audio):
+        with torch.no_grad():
+            fname = self.save(audio)
+            waveform, _ = torchaudio.load(fname, normalize=False)  # don't normalize on train
+            # waveform = waveform.type('torch.FloatTensor')
+            mfcc = self.featurizer(waveform).transpose(1, 2).transpose(0, 1)
+
+            # TODO: read from buffer instead of saving and loading file
+            # waveform = torch.Tensor([np.frombuffer(a, dtype=np.int16) for a in audio]).flatten()
+            # mfcc = self.featurizer(waveform).transpose(0, 1).unsqueeze(1)
+
+            out = self.model(mfcc)
+            pred = torch.round(torch.sigmoid(out))
+            print(pred.item())
+            return pred.item()
+
+    def inference_loop(self, action):
+        while True:
+            if len(self.audio_q) > 15:  # remove part of stream
+                diff = len(self.audio_q) - 15
+                for _ in range(diff):
+                    self.audio_q.pop(0)
+                action(self.predict(self.audio_q))
+            elif len(self.audio_q) == 15:
+                action(self.predict(self.audio_q))
+            time.sleep(0.05)
+
+    def run(self, action):
+        self.listener.run(self.audio_q)
+        thread = threading.Thread(target=self.inference_loop,
+                                    args=(action,), daemon=True)
+        thread.start()
+
+
+class DemoAction:
+    """This demo action will just randomly say Arnold Schwarzenegger quotes
+
+        args: sensitivty. the lower the number the more sensitive the
+        wakeword is to activation.
+    """
+    def __init__(self, sensitivity):
+        # import stuff here to prevent engine.py from 
+        # importing unecessary modules during production usage
+        import os
+        import subprocess
+        import random
+        from os.path import join, realpath
+
+        self.random = random
+        self.subprocess = subprocess
+        self.detect_in_row = 0
+
+        self.sensitivity = sensitivity
+        folder = realpath(join(realpath(__file__), '..', '..', '..', 'demo_voice', 'terminator'))
+        self.arnold_mp3 = [
+            os.path.join(folder, x)
+            for x in os.listdir(folder)
+            if ".wav" in x
+        ]
+
+    def __call__(self, prediction):
+        if prediction == 1:
+            self.detect_in_row += 1
+            if self.detect_in_row == self.sensitivity:
+                self.play()
+                self.detect_in_row = 0
+        else:
+            self.detect_in_row = 0
+
+    def play(self):
+        filename = self.random.choice(self.arnold_mp3)
+        try:
+            print("playing", filename)
+            self.subprocess.check_output(['play', '-v', '.1', filename])
+        except Exception as e:
+            print(str(e))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="demoing the wakeword engine")
+    parser.add_argument('--model_file', type=str, default=None, required=True,
+                        help='optimized file to load. use optimize_graph.py')
+    parser.add_argument('--sensitivity', type=int, default=10, required=False,
+                        help='lower value is more sensitive to activations')
+
+    args = parser.parse_args()
+    wakeword_engine = WakeWordEngine(args.model_file)
+    action = DemoAction(args.sensitivity)
+
+    print("""\n*** Make sure you have sox installed on your system for the demo to work!!!
+    If you don't want to use sox, change the play function in the DemoAction class
+    in engine.py module to something that works with your system.\n
+    """)
+    # action = lambda x: print(x)
+    wakeword_engine.run(action)
+    threading.Event().wait()
---
+++ AI-Voice-Assistant/wakeword/scripts/create_wakeword_jsons.py
@@ -0,0 +1,70 @@
+"""Utility script to create training json file for wakeword.
+
+    There should be two directories. one that has all of the 0 labels
+    and one with all the 1 labels
+"""
+import os
+import argparse
+import json
+import random
+
+
+def main(args):
+    zeros = os.listdir(args.zero_label_dir)
+    ones = os.listdir(args.one_label_dir)
+    percent = args.percent
+    data = []
+    for z in zeros:
+        data.append({
+            "key": os.path.join(args.zero_label_dir, z),
+            "label": 0
+        })
+    for o in ones:
+        data.append({
+            "key": os.path.join(args.one_label_dir, o),
+            "label": 1
+        })
+    random.shuffle(data)
+
+    f = open(args.save_json_path +"/"+ "train.json", "w")
+    
+    with open(args.save_json_path +"/"+ 'train.json','w') as f:
+        d = len(data)
+        i=0
+        while(i<int(d - ((d / 100)* percent))):
+            r=data[i]
+            line = json.dumps(r)
+            f.write(line + "\n")
+            i = i+1
+    
+    f = open(args.save_json_path +"/"+ "test.json", "w")
+
+    with open(args.save_json_path +"/"+ 'test.json','w') as f:
+        d = len(data)
+        i=int(d - ((d / 100)* percent))
+        while(i<d):
+            r=data[i]
+            line = json.dumps(r)
+            f.write(line + "\n")
+            i = i+1
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="""
+    Utility script to create training json file for wakeword.
+
+    There should be two directories. one that has all of the 0 labels
+    and one with all the 1 labels
+    """
+    )
+    parser.add_argument('--zero_label_dir', type=str, default=None, required=True,
+                        help='directory of clips with zero labels')
+    parser.add_argument('--one_label_dir', type=str, default=None, required=True,
+                        help='directory of clips with one labels')
+    parser.add_argument('--save_json_path', type=str, default=None, required=True,
+                        help='path to save json file')
+    parser.add_argument('--percent', type=int, default=10, required=False,
+                        help='percent of clips put into test.json instead of train.json')
+    args = parser.parse_args()
+
+    main(args)
